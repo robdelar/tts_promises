d0c2b6e6a888ec3d0f50029995d010fc2b9e2f0d
robdelar <44275659+robdelar@users.noreply.github.com>
2022-08-08 22:20:07 -0700

    Update async.js

--- async.js
+++ async.js
@@ -1,1 +1,12 @@
 
+async function f(cityName){
+		
+	let response = await fetch(`https://geocode.xyz/${cityName}?json=1`);
+  
+  let user = await response.json();
+  
+  console.log(user.latt);
+  console.log(user.longt);
+}
+
+f("dallas");
