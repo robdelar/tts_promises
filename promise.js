abe1a673bea732dea6f487a92111d9090183d0da
robdelar <44275659+robdelar@users.noreply.github.com>
2022-08-08 21:13:37 -0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,1 +1,15 @@
 
+function getRandomNumber() {
+        return new Promise((resolve) => {
+     setTimeout(() => resolve(Math.floor(Math.random()*100)), 500)
+     });
+ }
+    
+
+async function f(){
+	let result = await getRandomNumber();
+  
+  console.log(result);
+}
+
+f();
